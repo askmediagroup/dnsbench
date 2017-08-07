03a90f7c1279de939b2543322e30f95461930b54
Richard Marshall <richard.marshall@iacpub.com>
2017-08-07 09:55:35 -0700

    Update reporting and add local resolver mode

--- dnsbench.go
+++ dnsbench.go
@@ -4,13 +4,18 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
-	"github.com/miekg/dns"
 	"log"
 	"math/rand"
 	"net"
 	"os"
+	"path"
 	"time"
+
+	"github.com/codahale/hdrhistogram"
+	"github.com/miekg/dns"
 )
+
+const ms float64 = 1000000
 
 type Result struct {
 	duration time.Duration
@@ -18,31 +23,24 @@ type Result struct {
 }
 
 type Stats struct {
-	errors  int
-	average int64
-	dur     int64
-	max     int64
-	min     int64
+	errors    int64
+	hist      *hdrhistogram.Histogram
+	intErrors int64
+	intHist   *hdrhistogram.Histogram
 }
 
-func (s *Stats) Update(newRTT time.Duration, n int64) {
-	s.average = (int64(newRTT) + (n * int64(s.average))) / (n + 1)
-	s.dur += int64(newRTT)
-	if int64(newRTT) > s.max {
-		s.max = int64(newRTT)
-	}
-	if s.min == 0 || int64(newRTT) < s.min {
-		s.min = int64(newRTT)
-	}
-}
-
-func resolve(nameserver string, names []string, count int, results chan<- *Result) {
+func resolve(nameserver string, names []string, count int, qps int, results chan<- *Result) {
 	var result *Result
+	var sleep time.Duration
 	indexes := make([]int32, count)
 	c := new(dns.Client)
+	c.Timeout = time.Second * 5
 	m := new(dns.Msg)
 	for i := range indexes {
 		indexes[i] = rand.Int31n(int32(len(names)))
+	}
+	if qps > 0 {
+		sleep = time.Duration(int(time.Second) / qps)
 	}
 
 	for i := 0; i < count; i++ {
@@ -54,6 +52,30 @@ func resolve(nameserver string, names []string, count int, results chan<- *Resul
 			result.error = true
 		}
 		results <- result
+		time.Sleep(sleep)
+	}
+}
+
+func localResolve(names []string, count int, qps int, results chan<- *Result) {
+	var result *Result
+	var sleep time.Duration
+	indexes := make([]int32, count)
+	for i := range indexes {
+		indexes[i] = rand.Int31n(int32(len(names)))
+	}
+	if qps > 0 {
+		sleep = time.Duration(int(time.Second) / qps)
+	}
+	for i := 0; i < count; i++ {
+		start := time.Now()
+		_, err := net.LookupHost(names[indexes[i]])
+		result = new(Result)
+		result.duration = time.Since(start)
+		if err != nil {
+			result.error = true
+		}
+		results <- result
+		time.Sleep(sleep)
 	}
 }
 
@@ -78,62 +100,106 @@ func readNames(path string) ([]string, error) {
 	return names, scanner.Err()
 }
 
-func main() {
-	var count int
-	var concur int
-	var namesfile string
-	var nameserver string
-	var result *Result
-	stats := Stats{}
+func displayReport(hist *hdrhistogram.Histogram, errors int64, dur float64) {
+	fmt.Printf("%7d %7d %6.2f    [%4.2f %4.2f %4.2f %5.2f] %3.2f  %3.2f\n",
+		hist.TotalCount(),
+		errors,
+		float64(hist.Min())/ms,
+		float64(hist.ValueAtQuantile(50))/ms,
+		float64(hist.ValueAtQuantile(95))/ms,
+		float64(hist.ValueAtQuantile(99))/ms,
+		float64(hist.ValueAtQuantile(999))/ms,
+		float64(hist.Max())/ms,
+		float64(hist.TotalCount())/dur,
+	)
 
-	flag.IntVar(&count, "count", 1000, "Number of requests to make")
-	flag.IntVar(&concur, "concurency", 10, "Numer of concurrent requests")
-	flag.StringVar(&namesfile, "names", "-", "File containing names to request")
-	flag.StringVar(&nameserver, "nameserver", "", "Nameserver to query")
+}
+
+func main() {
+	var nameserver string
+	stats := Stats{}
+	stats.hist = hdrhistogram.New(0, 5e+9, 3)
+	stats.intHist = hdrhistogram.New(0, 5e+9, 3)
+
+	count := flag.Int("count", 1000, "Number of requests to make")
+	interval := flag.Duration("interval", 5*time.Second, "Reporting interval")
+	concur := flag.Int("concurrency", 10, "Numer of concurrent requests")
+	namesfile := flag.String("names", "-", "File containing names to request")
+	localResolver := flag.Bool("local", false, "Use local resolver")
+	qps := flag.Int("qps", 0, "QPS to attempt for each worker (0 for no limit)")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s <nameserver> [flags]\n", path.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
 	flag.Parse()
-	results := make(chan *Result, count)
-	names, err := readNames(namesfile)
+	if !*localResolver {
+		if flag.NArg() != 1 {
+			flag.Usage()
+			os.Exit(2)
+		}
+		nameserver = flag.Arg(0)
+	}
+	results := make(chan *Result, *concur)
+	cleanup := make(chan bool, 2)
+	names, err := readNames(*namesfile)
 	if err != nil {
 		log.Fatalf("Unable to parse names list: %s", err)
 	}
-
-	fmt.Printf("Benchmarking %s...\n\n", nameserver)
-	perworker := count / concur
-	perworkerrem := count % concur
+	timeout := time.After(*interval)
+	if *localResolver {
+		fmt.Printf("Benchmarking...\n\n")
+	} else {
+		fmt.Printf("Benchmarking %s...\n\n", nameserver)
+	}
+	perworker := *count / *concur
+	perworkerrem := *count % *concur
 	start := time.Now().UnixNano()
-	for i := 0; i < concur; i++ {
+	for i := 0; i < *concur; i++ {
 		t := perworker
 		if i < perworkerrem {
 			t += 1
 		}
-		go resolve(nameserver, names, t, results)
-	}
-
-	mod := 100
-	if count >= 1000 {
-		mod = count / 10
-	}
-
-	for j := 0; j < count; j++ {
-		if j > 0 && j%mod == 0 {
-			fmt.Printf("Completed %d requests\n", j)
-		}
-		result = <-results
-		stats.Update(result.duration, int64(j))
-		if result.error {
-			stats.errors++
+		if *localResolver {
+			go localResolve(names, t, *qps, results)
+		} else {
+			go resolve(nameserver, names, t, *qps, results)
 		}
 	}
-	end := time.Now().UnixNano()
-	duration := float64(end-start) / float64(time.Second)
 
-	fmt.Printf("Finished %d requests\n\n", count)
-	fmt.Printf("Concurrency level: %d\n", concur)
-	fmt.Printf("Time taken for tests: %.2f seconds\n", duration)
-	fmt.Printf("Completed Requests: %d\n", count-stats.errors)
-	fmt.Printf("Failed Requests: %d\n", stats.errors)
-	fmt.Printf("Requests per second: %.2f [#/sec] (mean)\n", float64(count)/duration)
-	fmt.Printf("Time per request: %.2f [ms] (mean)\n", float64(stats.average)/float64(time.Millisecond))
-	fmt.Printf("Fastest request: %.2f [ms]\n", float64(stats.min)/float64(time.Millisecond))
-	fmt.Printf("Slowest request: %.2f [ms]\n", float64(stats.max)/float64(time.Millisecond))
+	fmt.Printf("# requests errors min  [ p50  p95  p99  p999] max  qps\n")
+	for {
+		select {
+		case result := <-results:
+			stats.hist.RecordValue(int64(result.duration))
+			stats.intHist.RecordValue(int64(result.duration))
+			if result.error {
+				stats.errors++
+				stats.intErrors++
+			}
+			if stats.hist.TotalCount() >= int64(*count) {
+				cleanup <- true
+			}
+		case <-timeout:
+			displayReport(stats.intHist, stats.intErrors, float64(*interval)/float64(time.Second))
+			stats.intHist.Reset()
+			stats.intErrors = 0
+			timeout = time.After(*interval)
+		case <-cleanup:
+			end := time.Now().UnixNano()
+			duration := float64(end-start) / float64(time.Second)
+
+			fmt.Printf("\nFinished %d requests\n\n", *count)
+			fmt.Printf("# latency summary\n")
+			displayReport(stats.hist, stats.errors, duration)
+			fmt.Printf("\nConcurrency level: %d\n", *concur)
+			fmt.Printf("Time taken for tests: %.2f seconds\n", duration)
+			fmt.Printf("Completed Requests: %d\n", int64(*count)-stats.errors)
+			fmt.Printf("Failed Requests: %d\n", stats.errors)
+			fmt.Printf("Requests per second: %.2f [#/sec] (mean)\n", float64(*count)/duration)
+			fmt.Printf("Time per request: %.2f [ms] (mean)\n", float64(stats.hist.Mean())/1000000)
+			fmt.Printf("Fastest request: %.2f [ms]\n", float64(stats.hist.Min())/1000000)
+			fmt.Printf("Slowest request: %.2f [ms]\n", float64(stats.hist.Max())/1000000)
+			return
+		}
+	}
 }
